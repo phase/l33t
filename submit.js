8299fc92ca32b5def45e6bee22d0ebfc9ec599a9
Jadon Fowler <ph4sesaber@gmail.com>
2015-02-27 11:59:32 -0800

    Update submit.js

--- submit.js
+++ submit.js
@@ -20,6 +20,7 @@ function getRadioCheckedValue(radio_name){
  for(var i = 0; i < oRadio.length; i++){
 
    if(oRadio[i].checked){
+         console.log(oRadio[i].value);
          return parseFloat(oRadio[i].value);
    }
  }
